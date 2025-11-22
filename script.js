b4b2045130895d85236c075901d7de7d0c894030
mannatttarora27 <aroramannat006@gmail.com>
2025-11-22 11:48:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('more-btn').addEventListener('click', function() {
+    document.getElementById('extra-info').style.display = 'block';
+});
+
+document.getElementById('hide-btn').addEventListener('click', function() {
+    document.getElementById('extra-info').style.display = 'none';
+});
